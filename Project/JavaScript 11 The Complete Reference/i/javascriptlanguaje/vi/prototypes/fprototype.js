be970f5a22c3b424f16e1fc0033e582c84ee5c82
ruiz211 <ruiz.im211@gmail.com>
2021-03-01 16:37:41 -0500

    F.prototype

---
+++ Project/JavaScript 11 The Complete Reference/i/javascriptlanguaje/vi/prototypes/fprototype.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let animal = {
+    eats: true
+};
+
+function Rabbit(name) {
+    this.name = name;
+}
+
+Rabbit.prototype = animal;
+
+let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
+
+alert(rabbit.eats); // true
