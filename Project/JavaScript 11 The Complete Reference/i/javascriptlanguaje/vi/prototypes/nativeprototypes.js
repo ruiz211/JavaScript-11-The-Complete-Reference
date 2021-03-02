463c303471ddaec29a098324af8372abbd5892a5
ruiz211 <ruiz.im211@gmail.com>
2021-03-02 09:48:17 -0500

    Native prototypes

---
+++ Project/JavaScript 11 The Complete Reference/i/javascriptlanguaje/vi/prototypes/nativeprototypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let object = {};
+
+console.log(object.constructor);
+console.log(object.toString());
+
+console.log(object.toString == Object.prototype.toString);
+
+console.dir([1, 2, 3]);
